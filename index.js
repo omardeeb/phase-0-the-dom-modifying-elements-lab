f67672b13c80853b69fb6f3785d3919f83ee84dd
oamr <or.deeb@gmail.com>
2022-10-14 11:45:04 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.append(newHeader);
+newHeader = document.getElementById("victory");
+const node = document.createTextNode("Omar is the champion");
+newHeader.appendChild(node);
+const element = document.getElementById("victory");
+element.appendChild(newHeader);
+
